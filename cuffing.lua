8e70ac03c44c4c9cb4ff40c37696d2d13e929b84
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2023-05-08 15:57:51 +0100

    Removed thread (reformatted)

--- cuffing.lua
+++ cuffing.lua
@@ -24,57 +24,57 @@ RegisterNetEvent("police:client:breakout", function()
     end, math.random(1, 3), 30)
 end)
 
-CreateThread(function()
-    local Cuffs = {
-        {
-            name = 'handcuff:player',
-            event = 'police:client:CuffPlayerSoft',
-            icon = 'fa-solid fa-handcuffs',
-            label = 'Handcuff',
-            distance = 2.0,
-            items = {Config.HandCuffItem},
-        }
+local function()
+local Cuffs = {
+    {
+        name = 'handcuff:player',
+        event = 'police:client:CuffPlayerSoft',
+        icon = 'fa-solid fa-handcuffs',
+        label = 'Handcuff',
+        distance = 2.0,
+        items = {Config.HandCuffItem},
     }
-    local Uncuff = {
-        { -- Lockpicks Player Cuffs
-            name = 'uncuff:player',
-            event = 'police:client:breakout',
-            icon = 'fa-solid fa-handcuffs',
-            label = 'Uncuff',
-            items = Config.LockpickItem,
-            distance = 2.0,
-        },
-        { -- Police Cuff removal
-            name = 'uncuff:player',
-            event = 'police:client:CuffPlayerSoft',
-            icon = 'fa-solid fa-handcuffs',
-            label = 'Uncuff',
-            groups = Config.Policejobs,
-            distance = 2.0,
-        },
-    }
-    local CarOptions = {
-        {
-            name = 'vehicle:impound',
-            event = 'police:client:impounded',
-            icon = 'fa-sharp fa-solid fa-car-side',
-            label = 'Impound Vehicle',
-            groups = Config.Policejobs,
-            distance = 2.0,
-        },
-        {
-            name = 'vehicle:depot',
-            event = 'police:client:impounded',
-            icon = 'fa-sharp fa-solid fa-car-side',
-            label = 'Depot Vehicle',
-            groups = Config.Policejobs,
-            distance = 2.0,
-        },
-    }
+}
+local Uncuff = {
+    { -- Lockpicks Player Cuffs
+        name = 'uncuff:player',
+        event = 'police:client:breakout',
+        icon = 'fa-solid fa-handcuffs',
+        label = 'Uncuff',
+        items = Config.LockpickItem,
+        distance = 2.0,
+    },
+    { -- Police Cuff removal
+        name = 'uncuff:player',
+        event = 'police:client:CuffPlayerSoft',
+        icon = 'fa-solid fa-handcuffs',
+        label = 'Uncuff',
+        groups = Config.Policejobs,
+        distance = 2.0,
+    },
+}
+local CarOptions = {
+{
+    name = 'vehicle:impound',
+    event = 'police:client:impounded',
+    icon = 'fa-sharp fa-solid fa-car-side',
+    label = 'Impound Vehicle',
+    groups = Config.Policejobs,
+    distance = 2.0,
+},
+{
+    name = 'vehicle:depot',
+    event = 'police:client:impounded',
+    icon = 'fa-sharp fa-solid fa-car-side',
+    label = 'Depot Vehicle',
+    groups = Config.Policejobs,
+    distance = 2.0,
+},
+}
     exports.ox_target:addGlobalPlayer(Cuffs)
     exports.ox_target:addGlobalVehicle(CarOptions)
     if PlayerData.metadata.ishandcuffed then
         exports.ox_target:removeGlobalPlayer(Cuffs)
         exports.ox_target:addGlobalPlayer(Uncuff)
     end
-end)
+end
